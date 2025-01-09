6b0639819e713d043212a90635791ac2a9d5a6c6
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2025-01-08 23:45:45 -0500

    CatDB: update setup settings

---
+++ python-package/setup.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pip
+from setuptools import setup
+from setuptools.command.install import install
+
+requirements = [
+        "fancyimpute == 0.7.0",
+        "impyute == 0.0.8",
+        "jellyfish == 0.6.1",
+        "joblib == 1.4.2",
+        "matplotlib == 3.5.3",
+        "numpy == 1.22.4",
+        "pandas==2.1.4",
+        "py_stringmatching==0.4.6",
+        "py_stringsimjoin==0.3.6",
+        "scipy == 1.11.3",
+        "seaborn == 0.13.2",
+        "scikit_learn==1.6.0",
+        #sklearn_contrib_py_earth == 0.1.0
+        "statsmodels == 0.13.5",
+        "tdda == 2.0.9"
+]
+
+
+class OverrideInstallCommand(install):
+    def run(self):
+        # Install all requirements
+        failed = []
+
+        # for req in requirements:
+            # if pip.main(["install", req]) == 1:
+            #    failed.append(req)
+
+        if len(failed) > 0:
+            print("")
+            print("Error installing the following packages:")
+            print(str(failed))
+            print("Please install them manually")
+            print("")
+            raise OSError("Aborting")
+
+        # install Learn2Clean
+        install.run(self)
+
+
+with open("README.rst","r") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name='learn2clean',
+    version='0.2.1',
+    description="Python Library for Data Preprocessing with Reinforcement "
+    "Learning.",
+    long_description=readme,
+    author="Laure BERTI-EQUILLE",
+    author_email='laure.berti@ird.fr',
+    url='https://github.com/LaureBerti/learn2clean',
+    packages=[
+        'learn2clean',
+        'learn2clean.loading',
+        'learn2clean.normalization',
+        'learn2clean.consistency_checking',
+        'learn2clean.duplicate_detection',
+        'learn2clean.imputation',
+        'learn2clean.outlier_detection',
+        'learn2clean.feature_selection',
+        'learn2clean.qlearning',
+        #'learn2clean.classification',
+        #'learn2clean.regression',
+        #'learn2clean.clustering'
+        ],
+    package_dir={
+        'learn2clean': 'learn2clean',
+        'learn2clean.loading': 'learn2clean/loading',
+        'learn2clean.normalization': 'learn2clean/normalization',
+        'learn2clean.consistency_checking': 'learn2clean/consistency_checking',
+        'learn2clean.duplicate_detection': 'learn2clean/duplicate_detection',
+        'learn2clean.imputation': 'learn2clean/imputation',
+        'learn2clean.outlier_detection': 'learn2clean/outlier_detection',
+        'learn2clean.feature_selection': 'learn2clean/feature_selection',
+        'learn2clean.qlearning': 'learn2clean/qlearning',
+        #'learn2clean.classification': 'learn2clean/classification',
+        #'learn2clean.regression': 'learn2clean/regression',
+        #'learn2clean.clustering': 'learn2clean/clustering'
+    },
+    include_package_data=True,
+    cmdclass={
+        'install': OverrideInstallCommand},
+    install_requires=requirements,
+    zip_safe=False,
+    license='BSD-3',
+    keywords='learn2clean data preprocessing pipeline',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3.10'],
+    test_suite='tests',
+    tests_require=requirements)

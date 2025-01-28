1dc7d17ff2203b5749bac3c130d148750894bcfc
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2025-01-28 03:12:00 -0500

    CatDB: minor

--- python-package/learn2clean/loading/reader.py
+++ python-package/learn2clean/loading/reader.py
@@ -488,8 +488,8 @@ class Reader():
                            df_train[path] = df[~is_null].drop(target_name, axis=1)
                            y_train[path] = df[target_name][~is_null]
                         else:
-                            y_test[path] = df[target_name]
-                            df_test[path] = df.drop(target_name, axis=1)
+                            y_test[path] = df[target_name][~is_null]
+                            df_test[path] = df[~is_null].drop(target_name, axis=1)
 
 
                         # y_test[path] = y_train[path]
